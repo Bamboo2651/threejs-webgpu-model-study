47bf6449484248a72eeab1429206e55301e6a018
tkhy2651 <tah331925@gmail.com>
2026-04-01 21:10:22 +0900

    js

--- src/main.js
+++ src/main.js
@@ -1,39 +1,38 @@
 import * as THREE from 'three/webgpu';
 
-//レンダー WEBGPU
-const renderer = new THREE.WebGPURenderer({ antialias: true });
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
+async function init() {
+    const renderer = new THREE.WebGPURenderer({ antialias: true });
+    await renderer.init();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    document.body.appendChild(renderer.domElement);
+    
+    const scene = new THREE.Scene();
+    const camera = new THREE.PerspectiveCamera(60, window.innerWidth / window.innerHeight, 0.1, 1000);
+    camera.position.set(0, 0, 5);
 
-//カメラ
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(60, window.innerWidth / window.innerHeight, 0.1, 100);
-camera.position.set(0, 0, 3);
+    const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
+    scene.add(ambientLight);
 
-// ライト
-const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
-scene.add(ambientLight);
+    const dirLight = new THREE.DirectionalLight(0xffffff, 1.5);
+    dirLight.position.set(5, 10, 5);
+    scene.add(dirLight);
 
-const dirLight = new THREE.DirectionalLight(0xffffff, 1.5);
-dirLight.position.set(5, 10, 5);
-scene.add(dirLight);
 
-// キューブ
-const geometry = new THREE.BoxGeometry();
-const material = new THREE.MeshStandardMaterial({ color: 0xffffff });
-const cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
+    const geometry = new THREE.BoxGeometry();
+    const material = new THREE.MeshStandardMaterial({ color: 0xffffff });
+    const cube = new THREE.Mesh(geometry, material);
+    cube.position.set(0, 1, 0);
+    scene.add(cube);
 
-// サイズ
-window.addEventListener('resize', () => {
-  camera.aspect = window.innerWidth / window.innerHeight;
-  camera.updateProjectionMatrix();
-  renderer.setSize(window.innerWidth, window.innerHeight);
-});
-
-// ループ
-renderer.setAnimationLoop(() => {
-  cube.rotation.x += 0.03;
-  cube.rotation.y += 0.01;
-  renderer.render(scene, camera);
-});
+    window.addEventListener('resize', () => {
+        camera.aspect = window.innerWidth / window.innerHeight;
+        camera.updateProjectionMatrix();
+        renderer.setSize(window.innerWidth, window.innerHeight);
+    });
+    renderer.setAnimationLoop(() => {
+        cube.rotation.x += 0.03;
+        cube.rotation.y += 0.01;
+        renderer.render(scene, camera);
+    });
+}
+init()
