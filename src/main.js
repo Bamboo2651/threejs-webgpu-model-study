2bbb4a8b733839c62befb6cd48d5d92ccbce6520
tkhy2651 <tah331925@gmail.com>
2026-04-02 13:04:47 +0900

    回転、

--- src/main.js
+++ src/main.js
@@ -6,7 +6,7 @@ async function init() {
     await renderer.init();
     renderer.setSize(window.innerWidth, window.innerHeight);
     document.body.appendChild(renderer.domElement);
-    
+
     const scene = new THREE.Scene();
     const camera = new THREE.PerspectiveCamera(60, window.innerWidth / window.innerHeight, 0.1, 1000);
     camera.position.set(0, 0, 5);
@@ -26,8 +26,19 @@ async function init() {
     // scene.add(cube);
     const loader = new GLTFLoader();
     const gltf = await loader.loadAsync('/public/models/クリスタル本体.glb');
+    const gltf2 = await loader.loadAsync('/public/models/クリスタル本体.glb');
+    const gltf3 = await loader.loadAsync('/public/models/クリスタル本体.glb');
+    gltf2.scene.scale.set(0.4, 0.4, 0.4);
+    gltf3.scene.scale.set(0.4, 0.4, 0.4);
+    // console.log(gltf2.scene);
+    gltf2.scene.position.set(1, -1, 0);
+    gltf3.scene.position.set(-1, -1, 0);
+    gltf2.scene.rotation.z = -40 * (Math.PI / 180);
+    gltf3.scene.rotation.z = 40 * (Math.PI / 180);
+    // gltf2.scene.rotation.z = -0.5;
+    // gltf3.scene.rotation.z = 0.5;
     gltf.scene.position.set(0, -1, 0);
-    scene.add(gltf.scene);
+    scene.add(gltf.scene, gltf2.scene, gltf3.scene);
 
     window.addEventListener('resize', () => {
         camera.aspect = window.innerWidth / window.innerHeight;
@@ -36,6 +47,8 @@ async function init() {
     });
     renderer.setAnimationLoop(() => {
         gltf.scene.rotation.y += 0.03;
+        // gltf2.scene.rotation.y += 0.01;
+        // gltf2.scene.rotation.z += 0.01;
         // gltf.rotation.y += 0.01;
         renderer.render(scene, camera);
     });
