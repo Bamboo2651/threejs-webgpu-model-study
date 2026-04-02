cdf923835b4a612166f878eba019ea03a8b1aa83
tkhy2651 <tah331925@gmail.com>
2026-04-02 14:22:07 +0900

    js

--- src/main.js
+++ src/main.js
@@ -13,12 +13,12 @@ async function init() {
     const camera = new THREE.PerspectiveCamera(60, window.innerWidth / window.innerHeight, 0.1, 1000);
     camera.position.set(0, 0, 5);
 
-    const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
-    scene.add(ambientLight);
+    // const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
+    // scene.add(ambientLight);
 
-    const dirLight = new THREE.DirectionalLight(0xffffff, 0);
-    dirLight.position.set(5, 10, 5);
-    scene.add(dirLight);
+    // const dirLight = new THREE.DirectionalLight(0xffffff, 0);
+    // dirLight.position.set(5, 10, 5);
+    // scene.add(dirLight);
 
     //環境
     const environment = new RoomEnvironment();
